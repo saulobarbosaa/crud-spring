59d90a4cb22c26b9053623f474eeec31d85a82f5
saulobarbosaa <saulolimaaa@gmail.com>
2023-04-03 15:52:00 -0300

    Implementação de teste - findById

---
+++ src/test/java/com/user/userapi/services/impl/UserServiceImplTest.java
@@ -0,0 +1,78 @@
+package com.user.userapi.services.impl;
+
+import com.user.userapi.dto.UserDTO;
+import com.user.userapi.models.UserModel;
+import com.user.userapi.repositories.UserRepository;
+import org.apache.catalina.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    public static final Integer ID      = 1;
+    public static final String NAME     = "cesar";
+    public static final String EMAIL    = "cesar@mail.com";
+    public static final String PASSWORD = "123";
+    @InjectMocks
+    private UserServiceImpl service;
+    @Mock
+    private UserRepository repository;
+    @Mock
+    private ModelMapper mapper;
+    UserDTO userDto;
+    private UserModel user;
+    private Optional<UserModel> optUser;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindUserByIdThenReturnAnUser() {
+        Mockito.when(repository.findById(Mockito.anyInt())).thenReturn(optUser);
+
+        UserModel response = service.findById(ID);
+
+        Assertions.assertNotNull(response);
+
+        //Validação de atributos
+        Assertions.assertEquals(ID, response.getId());
+        Assertions.assertEquals(NAME, response.getName());
+        Assertions.assertEquals(EMAIL, response.getEmail());
+        Assertions.assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new UserModel(ID, NAME, EMAIL, PASSWORD);
+        userDto = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optUser = Optional.of(new UserModel(ID, NAME, EMAIL, PASSWORD));
+    }
+}
